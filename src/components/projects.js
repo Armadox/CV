75ee3bc234edd37e3ee2e705f722cb8c6f7b7b5b
Armadox <114094525+Armadox@users.noreply.github.com>
2022-11-24 20:00:55 +0200

    first commit

--- src/components/projects.js
+++ src/components/projects.js
@@ -7,24 +7,6 @@ const MyProject = () => {
             <section className="part projects" id="projects">
                 <div className="container projects-part hidden">
                     <div className="row">
-                        <h3 className="col-xxl-5 col-xl-5 col-lg-6 projectName">Gym&More E-Commerce Site </h3>
-                        <div className="col-xxl-6 col-xl-6 col-lg-5"><hr/></div>
-                        <div className="col-lg-6"><img className='images'style={{marginTop : '15px'}}src={require('./style/img/gymsite.png')} /></div>
-                        <p className="col-lg-6">
-                            Dynamic Wordpress/WooCommerce website that was built with Wordpress and has it's own self made theme.<br/>
-                            The custom theme was built by reverse engineering and editing Wordpress php files.<br/>
-                            Website was built by mainly using Php, Javascript, jQuery, Bootstrap-5 and CSS.<br/>
-                            Functional AJAX cart that updates every time you add something in the cart.<br/>
-                            Project took around 2 weeks.<br/>
-                            <a href="http://adriandan1998.000webhostapp.com">Click to go to go to website !</a>
-                        </p>
-                    </div>
-                </div>
-           
-
-           
-                <div className="container projects-part hidden">
-                    <div className="row">
                         <h3 className="col-xxl-5 col-xl-5 col-lg-6 projectName">Test assignment from ScandiWeb</h3>
                         <div className="col-xxl-6 col-xl-6 col-lg-5"><hr/></div>
                         <div className="col-lg-6"><div className="testsite"><img className='testsite2 images2' src={require('./style/img/testsite.png')}></img></div></div>
@@ -36,6 +18,21 @@ const MyProject = () => {
                             Project took around 2 weeks while studying React and JSON.<br/>
                             Passed the test with 12/12 :D .<br/>
                             <a href="https://gym-and-more.ro">Click to go to go to website !</a>
+                        </p>
+                    </div>
+                </div>
+                <div className="container projects-part hidden">
+                    <div className="row">
+                        <h3 className="col-xxl-5 col-xl-5 col-lg-6 projectName">Gym&More E-Commerce Site </h3>
+                        <div className="col-xxl-6 col-xl-6 col-lg-5"><hr/></div>
+                        <div className="col-lg-6"><img className='images'style={{marginTop : '15px'}}src={require('./style/img/gymsite.png')} /></div>
+                        <p className="col-lg-6">
+                            Dynamic Wordpress/WooCommerce website that was built with Wordpress and has it's own self made theme.<br/>
+                            The custom theme was built by reverse engineering and editing Wordpress php files.<br/>
+                            Website was built by mainly using Php, Javascript, jQuery, Bootstrap-5 and CSS.<br/>
+                            Functional AJAX cart that updates every time you add something in the cart.<br/>
+                            Project took around 2 weeks.<br/>
+                            <a href="http://adriandan1998.000webhostapp.com">Click to go to go to website !</a>
                         </p>
                     </div>
                 </div>
